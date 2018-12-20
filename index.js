213f022ee7b1719a0c2a9bfd2cf0f7eb514ebe56
Dario Vladovic <d.vladimyr@gmail.com>
2018-12-20 01:34:17 +0100

    Implement GDrive client for querying file info :tada:
    - querying file info for all publicly shared items with graceful
      handling of missing and private ones
    - documenting and testing codebase extensively :blue_book:

---
+++ index.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const { URL } = require('url');
+const got = require('got');
+
+const { HTTPError } = got;
+const r = got.extend({
+  baseUrl: 'https://drive.google.com',
+  headers: { 'x-drive-first-party': 'DriveWebUi' }
+});
+
+const JSON_PADDING = ')]}\'';
+
+const isID = str => /^[a-zA-Z0-9_-]{8,64}$/.test(str);
+const isUrl = str => /^https?:\/\//.test(str);
+const isRedirect = code => code >= 300 & code < 400;
+
+/**
+ * Custom error class used for wrapping HTTP errors
+ * @class GDriveError
+ * @property {Error} reason original HTTP error
+ * @augments Error
+ */
+class GDriveError extends Error {
+  constructor(msg, reason) {
+    super(msg);
+    if (reason) this.reason = reason;
+  }
+}
+
+module.exports = {
+  GDriveError,
+  fetchInfo,
+  getItemId
+};
+
+/**
+ * @typedef {Object} FileInfo
+ * @param {String} disposition Google Drive scan status
+ * @param {String} fileName item filename
+ * @param {String} downloadUrl generated direct download url
+ * @param {String} scanResult Google Drive scan result
+ * @param {Number} sizeBytes file size
+ */
+
+/**
+ * Fetch file info for given item id
+ * @param {String} input Google Drive item view/open url or item id
+ * @returns {Promise<FileInfo>} item info
+ * @throws {GDriveError} throws on querying missing/private items
+ * @throws {TypeError} throws on invalid ID provided
+ *
+ * @example
+ * // fetch public video info
+ * const info = await fetchInfo('https://drive.google.com/open?id=1ObJEVgO6Y4cFjfxszUb1LhdyeKrq_wGD');
+ * console.log(info.downloadUrl);
+ * //=> https://doc-00-6c-docs.googleusercontent.com/docs/securesc/…/1ObJEVgO6Y4cFjfxszUb1LhdyeKrq_wGD
+ */
+async function fetchInfo(input) {
+  const id = isUrl(input) ? getItemId(input) : input;
+  if (!isID(id)) throw new TypeError('Invalid ID provided.');
+  const query = { id };
+  try {
+    const resp = await r.post('/uc', { query });
+    const json = resp.body.replace(JSON_PADDING, '');
+    return JSON.parse(json);
+  } catch (err) {
+    if (!(err instanceof HTTPError)) throw err;
+    if (err.statusCode === 404) {
+      throw new GDriveError('Item is not found.', err);
+    }
+    if (redirectsTo(err, 'accounts.google.com')) {
+      throw new GDriveError('Item is not accessible.', err);
+    }
+    throw new GDriveError('Failed to fetch info.', err);
+  }
+}
+
+/**
+ * Extract item id from Google Drive shareable link
+ * @param {String} url Google Drive item view/open url
+ * @returns {String} item id
+ *
+ * @example
+ * // with _open_ link
+ * const id = getItemId('https://drive.google.com/open?id=1ObJEVgO6Y4cFjfxszUb1LhdyeKrq_wGD');
+ * //=> 1ObJEVgO6Y4cFjfxszUb1LhdyeKrq_wGD
+ *
+ * // with _view_ link
+ * const id = getItemId('https://drive.google.com/file/d/1ObJEVgO6Y4cFjfxszUb1LhdyeKrq_wGD/view?usp=sharing');
+ * //=> 1ObJEVgO6Y4cFjfxszUb1LhdyeKrq_wGD
+ */
+function getItemId(url) {
+  const parsed = parseUrl(url);
+  const segments = parsed.pathname.split('/');
+  const route = segments[segments.length - 1];
+  if (route === 'open') return parsed.searchParams.get('id');
+  if (route === 'view') return segments[segments.length - 2];
+}
+
+function redirectsTo(err, hostname) {
+  return isRedirect(err.statusCode) &&
+    getHostname(err.response.headers.location) === hostname;
+}
+
+function getHostname(url) {
+  const parsed = parseUrl(url);
+  return parsed && parsed.hostname;
+}
+
+function parseUrl(url) {
+  try {
+    return new URL(url);
+  } catch (err) {}
+}
---
+++ test.js
@@ -0,0 +1,118 @@
+'use strict';
+
+const { GDriveError, getItemId, fetchInfo } = require('./');
+const test = require('tape');
+
+const isUrl = str => /^https?:\/\//.test(str);
+
+test('Extract item id from `open` link', t => {
+  const id = '1ObJEVgO6Y4cFjfxszUb1LhdyeKrq_wGD';
+  const url = `https://drive.google.com/open?id=${id}`;
+  t.plan(1);
+  t.equals(getItemId(url), id, `extracted id=${id}`);
+});
+
+test('Extract item id from `view` link', t => {
+  const id = '1ObJEVgO6Y4cFjfxszUb1LhdyeKrq_wGD';
+  const url = `https://drive.google.com/file/d/${id}/view?usp=sharing`;
+  t.plan(1);
+  t.equals(getItemId(url), id, `extracted id=${id}`);
+});
+
+test('Use invalid item id', async t => {
+  t.plan(1);
+  try {
+    await fetchInfo('Invalid **video** id');
+  } catch (err) {
+    t.equals(
+      err instanceof TypeError && err.message, 'Invalid ID provided.',
+      `throws TypeError: ${err.message}`
+    );
+  }
+});
+
+test('Fetch public video info', async t => {
+  const id = '1ObJEVgO6Y4cFjfxszUb1LhdyeKrq_wGD';
+  const url = `https://drive.google.com/open?id=${id}`;
+  const expected = {
+    disposition: 'TOO_LARGE',
+    fileName: 'big-buck-bunny-720p-h264.mp4',
+    scanResult: 'WARNING',
+    sizeBytes: 158008374
+  };
+  const info = await fetchInfo(url);
+  t.plan(3);
+  t.equals(info && info.sizeBytes, expected.sizeBytes, 'correct file fetched (size match)');
+  t.equals(info && info.fileName, expected.fileName, 'correct filename');
+  const downloadUrl = info && info.downloadUrl;
+  t.ok(
+    downloadUrl && isUrl(downloadUrl) && downloadUrl.endsWith(id),
+    `correct url=${downloadUrl}`
+  );
+});
+
+test('Fetch missing video info', async t => {
+  const url = 'https://drive.google.com/open?id=dummy_gO6Y4cFjfxszUb1LhdyeKrq_wGD';
+  t.plan(1);
+  try {
+    await fetchInfo(url);
+  } catch (err) {
+    t.equals(
+      err instanceof GDriveError && err.message, 'Item is not found.',
+      `throws GDriveError: ${err.message}`
+    );
+  }
+});
+
+test('Fetch private video info', async t => {
+  const url = 'https://drive.google.com/open?id=1-AszYwYOagB6hkvJVgz9cfkwd4nVG4rd';
+  t.plan(1);
+  try {
+    await fetchInfo(url);
+  } catch (err) {
+    t.equals(
+      err instanceof GDriveError && err.message, 'Item is not accessible.',
+      `throws GDriveError: ${err.message}`
+    );
+  }
+});
+
+test('Fetch picture info', async t => {
+  const id = '1X-1PiZWpgZrpmBcVpyUPSuz_7hI383LC';
+  const url = `https://drive.google.com/open?id=${id}`;
+  const expected = {
+    disposition: 'SCAN_CLEAN',
+    fileName: 'flower.png',
+    scanResult: 'OK',
+    sizeBytes: 114590
+  };
+  const info = await fetchInfo(url);
+  t.plan(3);
+  t.equals(info && info.sizeBytes, expected.sizeBytes, 'correct file fetched (size match)');
+  t.equals(info && info.fileName, expected.fileName, 'correct filename');
+  const downloadUrl = info && info.downloadUrl;
+  t.ok(
+    downloadUrl && isUrl(downloadUrl) && downloadUrl.endsWith(id),
+    `correct url=${downloadUrl}`
+  );
+});
+
+test('Fetch .txt file info', async t => {
+  const id = '1oTQi2sxyTRHHhKoviK_YJCO6nPh7Obmf';
+  const url = `https://drive.google.com/open?id=${id}`;
+  const expected = {
+    disposition: 'NO_SCAN_NEEDED',
+    fileName: 'dummy.txt',
+    scanResult: 'OK',
+    sizeBytes: 0
+  };
+  const info = await fetchInfo(url);
+  t.plan(3);
+  t.equals(info && info.sizeBytes, expected.sizeBytes, 'correct file fetched (size match)');
+  t.equals(info && info.fileName, expected.fileName, 'correct filename');
+  const downloadUrl = info && info.downloadUrl;
+  t.ok(
+    downloadUrl && isUrl(downloadUrl) && downloadUrl.endsWith(id),
+    `correct url=${downloadUrl}`
+  );
+});
